42c3e39e4aced42f1efc73df1de4493f951701d8
BananaSkiee <bananaskiee@gmail.com>
2026-03-07 03:51:26 +0700

    Update ContactTab.tsx

--- src/components/ContactTab.tsx
+++ src/components/ContactTab.tsx
@@ -11,20 +11,20 @@ const contacts: ContactLink[] = [
   {
     icon: <MessageCircle size={14} />,
     label: 'Discord',
-    value: 'arcticayl',
-    href: 'https://discord.com/users/arcticayl',
+    value: 'EmpireBS',
+    href: 'https://discord.gg/aTEGb6SaFQ',
   },
   {
     icon: <Github size={14} />,
     label: 'GitHub',
-    value: '@irenic.alyaa',
-    href: 'https://github.com/irenic.alyaa',
+    value: '@bananaskiee',
+    href: 'https://github.com/BananaSkiee',
   },
   {
     icon: <Mail size={14} />,
     label: 'Email',
-    value: 'businesswithmikasa@gmail.com',
-    href: 'mailto:businesswithmikasa@gmail.com',
+    value: 'bananaskiee@gmail.com',
+    href: 'mailto:bananaskiee@gmail.com',
   },
 ];
 
